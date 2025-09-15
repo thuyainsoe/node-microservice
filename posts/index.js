7064d3ae361d2201f5a4b299a6ffe2c1b949bc00
thuyainsoe <thuyainsoe163361@gmail.com>
2025-09-15 21:50:39 +0700

    learn microservice archi

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,26 +10,35 @@ app.use(cors());
 
 const posts = {};
 
-console.log(posts);
-
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   console.log(req.body, "req.body");
 
   const { title } = req.body;
 
-  console.log(title, "title");
-
   posts[id] = {
     id,
-    title, // why only id is showing in postman not title
+    title,
   };
 
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
   res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+  res.send({});
 });
 
 app.listen(4000, () => {
